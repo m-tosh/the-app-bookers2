e72bd42394a366760097050a12d02bfaf4e5e943
m-tosh <t.m.undefeated@gmail.com>
2021-10-08 15:40:36 +0000

    rspecテスト完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
-    redirect_to users_path if @user.id != current_user.id
+    redirect_to user_path(current_user.id) if @user.id != current_user.id
   end
 
   def update
