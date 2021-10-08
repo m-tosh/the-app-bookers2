aa661705b4a22f7f19f8a5634e5945d28d52f1b9
m-tosh <t.m.undefeated@gmail.com>
2021-10-08 09:02:04 +0000

    ヘッダーフッターのレイアウト修正

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
-  resources :users, only: [:show, :edit, :update]
+  resources :users, only: [:index, :show, :edit, :update]
   resources :books, only: [:create, :index, :show, :edit, :update, :destroy]
 end
