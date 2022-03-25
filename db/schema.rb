5c9cab6d0eaa684eac1adcd757c5c7e3ac02350e
toshiya <>
2022-03-25 16:42:29 +0900

    :fire: Userからprofile_image_idカラムを削除
    refileで使用していたが、ActiveStorageに移行したため。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_14_090852) do
+ActiveRecord::Schema.define(version: 2022_03_25_073804) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -49,7 +49,6 @@ ActiveRecord::Schema.define(version: 2022_03_14_090852) do
     t.datetime "remember_created_at"
     t.string "name"
     t.text "introduction"
-    t.string "profile_image_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
